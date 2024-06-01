a73fe875c78f658af5a39c3f6e2106e416e4010c
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2024-06-01 16:25:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -108,7 +109,12 @@ app.get('/search', async (req, res) => {
         return;
     }
 
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
 
     const encodedQuery = encodeURIComponent(query);
