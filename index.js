df8d6227d37664cafc15243f040e402fc3421d8b
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2024-06-01 12:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
     res.send(`
         <html>
             <head>
+			 <title>IMDB MOVIES & TV SHOWS RATINGS COUNT</title>
+      <link rel="icon" href="https://m.media-amazon.com/images/G/01/imdb/images-ANDW73HA/favicon_desktop_32x32._CB1582158068_.png" type="image/x-icon">
                 <style>
                     /* CSS Styles */
                     body {
@@ -26,16 +28,16 @@ app.get('/', (req, res) => {
                         display: flex;
                         flex-direction: column;
                         align-items: center;
-                        background-color: rgba(0 0 0 / 27%);
-                        padding: 20px;
-                        border-radius: 8px;
+                        background-color: rgba(225 168 168 / 16%);
+                        padding: 83px;
+                        border-radius: 39px;
                     }
                     input[type="text"] {
                         padding: 8px;
                         margin-bottom: 8px;
                         width: 300px;
-                        border: 1px solid #ccc;
-                        border-radius: 4px;
+                        border: 1px solid #283147;
+                        border-radius: 18px;
                         background-color: rgba(255, 255, 255, 0.8);
                         color: #333;
                     }
@@ -56,17 +58,45 @@ app.get('/', (req, res) => {
                         flex-direction: column;
                         align-items: center;
                     }
+					.container {
+                text-align: center;
+                background-color: #8cafe76b;
+                padding: 20px;
+                border-radius: 63px;
+            }
                 </style>
                 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
             </head>
             <body>
                 <form action="/search" method="get" class="space-y-2">
-                    <label for="query" class="text-lg font-semibold">IMDb Movie Ratings</label>
+   <div style="text-align: center;">
+  <label for="query" class="text-lg font-bold" style="color: black; text-decoration: underline;">IMDB MOVIES & TV SHOWS RATINGS COUNT</label>
+  <br>
+  <br>
+  <div style="text-align: center;">
+  <label for="query" class="text-lg font-bold" style="color: #813333e0;">VISITORS COUNT</label>
+  <br>
+    <a href="https://www.hitwebcounter.com" target="_blank">
+      <img src="https://hitwebcounter.com/counter/counter.php?page=12946004&amp;style=0006&amp;nbdigits=4&amp;type=page&amp;initCount=1000" title="Counter Widget" alt="Visit counter For Websites" border="0">
+    </a>
+  <br>
+					<div class="container" style="margin-top: 20px; text-align: center;">
+  <button style="background-color: #00000000; padding: 10px 20px; margin-right: 1px;">
+    <a href="https://yashwanthwebproject.netlify.app" style="color: black; text-decoration: none; font-size: 18px; font-weight: bold; display: block; background-color: inherit; border: 2px solid white; border-radius: 5px; padding: 5px;">
+      Web Development Projects
+    </a>
+  </button>
+</div>
+<br>
                     <input type="text" id="query" name="query" required placeholder="Movie name or movie name with ratings" class="px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-blue-400 focus:ring-blue-400">
-                    <button type="submit" class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Search</button>
-                </form>
+					<br>
+					<button type="submit" class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Search</button>
+				</form>
                 <div id="result" class="result-container"></div> <!-- This is where the screenshot will be displayed -->
-            </body>
+				<br>
+				<span class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">© ® Developed By Yashwanth R</span>
+</div>
+			</body>
         </html>
     `);
 });
@@ -97,6 +127,8 @@ app.get('/search', async (req, res) => {
 
         res.send(`
             <html>
+			<title>IMDB MOVIES & TV SHOWS RATINGS COUNT</title>
+      <link rel="icon" href="https://m.media-amazon.com/images/G/01/imdb/images-ANDW73HA/favicon_desktop_32x32._CB1582158068_.png" type="image/x-icon">
                 <style>
                     body {
                         font-family: 'Arial', sans-serif;
@@ -146,7 +178,7 @@ app.get('/search', async (req, res) => {
         `);
     } else {
         await browser.close();
-        res.status(500).send('IMDb ratings link not found');
+        res.status(500).send('Valid Search Only Acceptable');
     }
 });
 
